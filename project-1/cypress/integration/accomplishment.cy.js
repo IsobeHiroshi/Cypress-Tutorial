632edf9825c65a9434161795be449c41c9c714f8
IsobeHiroshi <isobe69b@gmail.com>
2022-12-10 17:12:25 +0900

    Complete project 1

--- project-1/cypress/integration/accomplishment.cy.js
+++ project-1/cypress/integration/accomplishment.cy.js
@@ -1,1 +1,34 @@
 /// <reference types="cypress" />
+
+describe("Accomplishment Dashboard", () => {
+  beforeEach(() => {
+    cy.visit("/accomplishments")
+  })
+
+  it("should showcase error if information is missing", () => {
+    cy.getByTestId("accomplishment-title-input").type("My Basketball Accomplishment")
+    cy.getByTestId("accomplishment-input").type("I made 10 threes in a row")
+    cy.contains("Submit Accomplishment").click()
+    cy.contains("Complete the items above to continue").should("be.visible")
+  })
+
+  it("should display validation component if all information is input", () => {
+    cy.getByTestId("accomplishment-title-input").type("My Basketball Accomplishment")
+    cy.getByTestId("accomplishment-input").type("I made 10 threes in a row")
+    cy.getByTestId("accomplishment-checkbox").click()
+    cy.contains("Submit Accomplishment").click()
+    cy.contains("This Accomplisment was Successfully Submitted").should("be.visible")
+  })
+
+  it("should return back to accomplishment dashboard with empty inputs when 'Go Back' button is clicked", () => {
+    cy.getByTestId("accomplishment-title-input").type("My Basketball Accomplishment")
+    cy.getByTestId("accomplishment-input").type("I made 10 threes in a row")
+    cy.getByTestId("accomplishment-checkbox").click()
+    cy.contains("Submit Accomplishment").click()
+    cy.contains("Go Back").click()
+    cy.contains("h2", "Accomplishment").should("be.visible")
+    cy.getByTestId("accomplishment-title-input").should("have.value", "")
+    cy.getByTestId("accomplishment-input").should("have.value", "")
+    cy.getByTestId("accomplishment-checkbox").should("not.be.checked")
+  })
+})
